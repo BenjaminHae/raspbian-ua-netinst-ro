4d0c11bbd772a463e5345e56e49f0fbd18edc7c6
Benjamin Häublein <benjaminhaeublein@gmail.com>
2016-12-21 18:19:50 +0100

    prepare poisontap

--- prepare_poison.sh
+++ prepare_poison.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
-sed -i "s|YOUR.DOMAIN|${host}|g" /var/opt/poisontap/target_backdoor.js
-
+#!/bin/sh
+if [ -e "/boot/host.poisontap" ]
+then
+    host=`cat /boot/host.poisontap`
+    sed -i "s|YOUR.DOMAIN|${host}|g" /var/opt/poisontap/target_backdoor.js
+    sed -i "s|YOUR.DOMAIN|${host}|g" /var/opt/poisontap/backdoor.html
+fi
+/var/opt/poisontap/pi_startup.sh
